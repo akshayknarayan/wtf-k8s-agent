5de84c234473b85d7fa2bae40510353fcb3b1e68
Elam Day-Friedland <elamdf@eecs.berkeley.edu>
2023-07-05 08:00:33 -0700

    some comments & fix example. Compile still broken on hashmap borrow issue

--- src/health.rs
+++ src/health.rs
@@ -17,6 +17,7 @@ enum SimplePodStatus {
     Failed,
     Unknown
 }
+
 impl fmt::Display for HealthBit {
         fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
             match self {
@@ -27,6 +28,7 @@ impl fmt::Display for HealthBit {
         }
     }
 }
+
 impl fmt::Display for SimplePodStatus  {
         fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
             match self {
@@ -38,6 +40,10 @@ impl fmt::Display for SimplePodStatus  {
             }
         }
 }
+
+// Get and map a pod's status to a health bit.
+// TODO this function should porbably map podstatus-> healthbit and
+// we'll have overloads (mapping other enums to healthbits) for other health metrics.
 pub async fn get_health_bit(pods: &Api<Pod>, pod_name: &String) -> Result<HealthBit, String> {
     match get_pod_status(pods, pod_name).await? {
         SimplePodStatus::Succeeded => Ok(HealthBit::Green),
@@ -47,14 +53,17 @@ pub async fn get_health_bit(pods: &Api<Pod>, pod_name: &String) -> Result<Health
         SimplePodStatus::Unknown => Ok(HealthBit::Red),
     }
 }
+
+// TODO regex these, maybe to determine ERRORs?
 pub async fn query_pod_logs(pods: &Api<Pod>, pod_name: &String) -> Result<(), String> {
     println!("{:?}", pods.logs(pod_name, &LogParams::default()).await.unwrap());
     Ok(())
 
 }
 
-async fn get_pod_status(pods: &Api<Pod>, pod_name: &String) -> Result<SimplePodStatus, String> {
-    let pod_status_str = match pods.get_status(pod_name).await {
+// Query kubes API to get the string status of a pod, and map it to a SimplePodStatus enum.
+async fn get_pod_status(pod_namespace: &Api<Pod>, pod_name: &String) -> Result<SimplePodStatus, String> {
+    let pod_status_str = match pod_namespace.get_status(pod_name).await {
         Ok(it) => it,
         Err(e) => return Err(format!("{} {}", "pod not found".to_string(), e)),
     }.status.unwrap().phase.unwrap();
