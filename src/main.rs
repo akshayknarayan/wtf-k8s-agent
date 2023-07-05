febdf743e9ee9283d4fb78d2791e71a676aead3e
Elam Day-Friedland <elamdf@eecs.berkeley.edu>
2023-07-05 08:00:33 -0700

    some comments & fix example. Compile still broken on hashmap borrow issue

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,8 @@ use kube::{api::Api, core::ObjectMeta, Client};
 use tracing::info;
 
 mod health;
-mod scope;
-
-// idea have a scope tracker that updates health bits for a scope on events related to pods within
-// that scope
+// TODO scope doesn't compile, don't really know how to fix the hashmap in-place iteration (over keys) & update
+// mod scope;
 
 fn main() -> Result<(), Report> {
     color_eyre::install()?;
@@ -24,13 +22,16 @@ fn main() -> Result<(), Report> {
 
         let pods: Api<Pod> = Api::default_namespaced(client.clone());
 
+        // TODO fix list hang (and then panic on timeout) here when we have paused pods? 
+        // is this inherent to the lib????
+        // 2023-07-05T01:37:59.868290Z ERROR kube_client::client::builder: failed with error error trying to connect: deadline has elapsed
         let _pods = pods.list(&Default::default()).await?;
 
         for p in _pods.items {
-        //let res = health::get_health_bits(&pods, &p.metadata.name.unwrap()).await;
-        let res = health::query_pod_logs(&pods, &p.metadata.name.unwrap()).await;
+        let res = health::get_health_bit(&pods, &p.metadata.name.clone().unwrap()).await;
+        //let res = health::query_pod_logs(&pods, &p.metadata.name.unwrap()).await;
         match res {
-        Ok(_) => (), 
+        Ok(bit) => println!("Pod: {} Health bit: {}", p.metadata.name.unwrap(), bit),
         Err(e) => println!("{}", e),
         }
         }
--- src/scope.rs
+++ src/scope.rs
@@ -5,7 +5,8 @@ use kube::Api;
 
 use crate::health::{HealthBit, get_health_bit};
 
-    // TODO should a scope encompass generic resources, rather than just pods? 
+// TODO should a scope encompass generic resources, rather than just pods? 
+// A scope of pods within a namespace, and a cache of their health bits.
 pub struct Scope {
     pods: HashMap<String, HealthBit>,
     namespace: Api<Pod>,
@@ -18,19 +19,24 @@ impl Scope {
         }
     }
 
+    // Add a pod to this scope.
     pub async fn add_to_scope(mut self: Scope, pod_name: &String) -> Result<(), String>{
      self.pods.insert(pod_name.to_string(), HealthBit::Unknown);
      Ok(())
     }
 
-    // TODO error return
-    pub async fn update_scope(self: Scope) -> () {
-        for pod_name in self.pods.keys().into_iter() {
-            self.query_pod(pod_name, Some(true));
+    // Update the cache of pod statuses within this scope.
+    // TODO this should be called on an event-watch loop and maybe either update a pod or its
+    // entire scope when an event (perhaps meeting some conditions) mentions a pod.
+    pub async fn update_scope(self: &Scope) -> () {
+        for (pod_name, _) in self.pods.clone() {
+            self.query_pod(&pod_name.to_owned(), Some(true)).await;
         }
     }
 
-    pub async fn query_pod(self: Scope, pod_name: &String, update: Option<bool>) -> Result<HealthBit, String> {
+    // Query the state of a pod within this scope. If `update` is false, return the cached status.
+    // Otherwise, query the state using the API.
+    pub async fn query_pod(mut self: Scope, pod_name: &String, update: Option<bool>) -> Result<HealthBit, String> {
         match self.pods.contains_key(pod_name) {
             true => {
                 match update.unwrap_or(false) {
@@ -44,7 +50,7 @@ impl Scope {
                 }
 
                 // TODO just derive copy trait?
-                // TODO prob don't use unwrap even though it's safe here
+                // TODO prob don't use unwrap even though it's safe here (bc match on contains_key)
                 Ok(self.pods.get(pod_name).unwrap().to_owned())
             }, 
             false => {
