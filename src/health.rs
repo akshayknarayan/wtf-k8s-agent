f21e59892ef02e54418c9fde81c5a9572e4e0335
Elam Day-Friedland <elamdf@eecs.berkeley.edu>
2023-07-06 22:47:00 -0700

    scope abstraction works with example

--- src/health.rs
+++ src/health.rs
@@ -2,43 +2,43 @@ use k8s_openapi::api::core::v1::{Event, Pod, PodStatus};
 use kube::api::{Api, LogParams};
 use std::fmt;
 
-#[derive(Clone)]
-pub enum HealthBit { 
+#[derive(Clone, Copy)]
+pub enum HealthBit {
     Red,
     Green,
     Yellow,
-    Unknown
+    Unknown,
 }
 
 enum SimplePodStatus {
-    Pending, 
+    Pending,
     Running,
     Succeeded,
     Failed,
-    Unknown
+    Unknown,
 }
 
 impl fmt::Display for HealthBit {
-        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-            match self {
-                HealthBit::Red =>  write!(f, "Red"),
-                HealthBit::Green =>  write!(f, "Green"),
-                HealthBit::Yellow =>  write!(f, "Yellow"),
-                HealthBit::Unknown =>  write!(f, "Unknown"),
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            HealthBit::Red => write!(f, "Red"),
+            HealthBit::Green => write!(f, "Green"),
+            HealthBit::Yellow => write!(f, "Yellow"),
+            HealthBit::Unknown => write!(f, "Unknown"),
         }
     }
 }
 
-impl fmt::Display for SimplePodStatus  {
-        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-            match self {
-                SimplePodStatus::Pending =>  write!(f, "Pending"),
-                SimplePodStatus::Succeeded =>  write!(f, "Succeeded"),
-                SimplePodStatus::Running =>  write!(f, "Running"),
-                SimplePodStatus::Failed =>  write!(f, "Failed"),
-                SimplePodStatus::Unknown =>  write!(f, "Unknown"),
-            }
+impl fmt::Display for SimplePodStatus {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            SimplePodStatus::Pending => write!(f, "Pending"),
+            SimplePodStatus::Succeeded => write!(f, "Succeeded"),
+            SimplePodStatus::Running => write!(f, "Running"),
+            SimplePodStatus::Failed => write!(f, "Failed"),
+            SimplePodStatus::Unknown => write!(f, "Unknown"),
         }
+    }
 }
 
 // Get and map a pod's status to a health bit.
@@ -56,23 +56,32 @@ pub async fn get_health_bit(pods: &Api<Pod>, pod_name: &String) -> Result<Health
 
 // TODO regex these, maybe to determine ERRORs?
 pub async fn query_pod_logs(pods: &Api<Pod>, pod_name: &String) -> Result<(), String> {
-    println!("{:?}", pods.logs(pod_name, &LogParams::default()).await.unwrap());
+    println!(
+        "{:?}",
+        pods.logs(pod_name, &LogParams::default()).await.unwrap()
+    );
     Ok(())
-
 }
 
 // Query kubes API to get the string status of a pod, and map it to a SimplePodStatus enum.
-async fn get_pod_status(pod_namespace: &Api<Pod>, pod_name: &String) -> Result<SimplePodStatus, String> {
+async fn get_pod_status(
+    pod_namespace: &Api<Pod>,
+    pod_name: &String,
+) -> Result<SimplePodStatus, String> {
     let pod_status_str = match pod_namespace.get_status(pod_name).await {
         Ok(it) => it,
         Err(e) => return Err(format!("{} {}", "pod not found".to_string(), e)),
-    }.status.unwrap().phase.unwrap();
+    }
+    .status
+    .unwrap()
+    .phase
+    .unwrap();
     match pod_status_str.as_ref() {
         "Succeeded" => Ok(SimplePodStatus::Succeeded),
         "Failed" => Ok(SimplePodStatus::Failed),
         "Running" => Ok(SimplePodStatus::Running),
         "Pending" => Ok(SimplePodStatus::Pending),
         "Unknown" => Ok(SimplePodStatus::Unknown),
-        e => Err(format!("unknown status: {}",e)),
+        e => Err(format!("unknown status: {}", e)),
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,17 @@ use std::fmt;
 
 use color_eyre::Report;
 use k8s_openapi::api::core::v1::{Event, Pod, PodStatus};
-use kube::{api::Api, core::ObjectMeta, Client};
+use kube::{
+    api::Api,
+    core::{ObjectList, ObjectMeta},
+    discovery::Scope,
+    Client,
+};
 use tracing::info;
 
 mod health;
 // TODO scope doesn't compile, don't really know how to fix the hashmap in-place iteration (over keys) & update
-// mod scope;
+mod wtf_scope;
 
 fn main() -> Result<(), Report> {
     color_eyre::install()?;
@@ -22,24 +27,31 @@ fn main() -> Result<(), Report> {
 
         let pods: Api<Pod> = Api::default_namespaced(client.clone());
 
-        // TODO fix list hang (and then panic on timeout) here when we have paused pods? 
-        // is this inherent to the lib????
-        // 2023-07-05T01:37:59.868290Z ERROR kube_client::client::builder: failed with error error trying to connect: deadline has elapsed
-        let _pods = pods.list(&Default::default()).await?;
+        let _pods = match pods.list(&Default::default()).await {
+            Ok(p) => p.into_iter(),
+            Err(_) => Vec::<Pod>::new().into_iter(),
+        };
 
-        for p in _pods.items {
-        let res = health::get_health_bit(&pods, &p.metadata.name.clone().unwrap()).await;
-        //let res = health::query_pod_logs(&pods, &p.metadata.name.unwrap()).await;
-        match res {
-        Ok(bit) => println!("Pod: {} Health bit: {}", p.metadata.name.unwrap(), bit),
-        Err(e) => println!("{}", e),
-        }
+        let mut scope = wtf_scope::WtfScope::new(pods);
+
+        for p in _pods {
+            match &p.metadata.name {
+                Some(name) => match scope.add_to_scope(name).await {
+                    Ok(_) => (),
+                    Err(e) => println!("error adding {} to scope: {}", name, e),
+                },
+                None => (),
+            };
         }
 
+        match scope.update_scope().await {
+            Ok(_) => (),
+            Err(e) => println!("error updating scope: {}", e),
+        };
+        println!("{}", scope);
 
         //let evs = events.list(&Default::default()).await?;
         //info!(?evs, "events");
-
 
         Ok(())
     })
